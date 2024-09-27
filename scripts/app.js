a30454b00598c965dbf9588b197039cd78b0cec0
psychotaha <tahaaslani84@gmail.com>
2024-09-27 18:10:44 +0330

    added some scripts to app and added empty error

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+document.getElementById("submittodo").addEventListener("click", function(event){
+    var todo_text = document.getElementById("inputtodo").value;
+    console.log(todo_text)
+})
+
+var todos = [];
+
+function addTodo(object){
+
+}
+
+function removeTodo(object){
+
+}
+
+function showTodo(object){
+    
+}
+
+function getTodos(object){
+
+}
